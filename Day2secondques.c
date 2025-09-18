c247163b8a1663b0fa07ec0a34169bc813ca6c0d
Palak-singh-11 <anthenasingh11@gmail.com>
2025-09-19 00:02:48 +0530

    Add files via upload

---
+++ Day2secondques.c
@@ -0,0 +1,8 @@
+#include<stdio.h>
+int main() {
+float radius,area,circumference;
+    printf("enter radius");
+scanf("%f", &radius);
+    printf("area of circle is: %f /n" , 3.14*radius*radius);
+    printf("perimeter of circle is: %f /n" , 2*3.14*radius);
+return 0; 
