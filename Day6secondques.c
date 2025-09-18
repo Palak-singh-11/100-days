a954af5d6147f9f7ac15e16902d892cd004e5905
Palak-singh-11 <anthenasingh11@gmail.com>
2025-09-19 00:50:05 +0530

    Add files via upload

---
+++ Day6secondques.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+int main() {
+    int num;
+
+    // Input from the user
+    printf("Enter an integer: ");
+    scanf("%d", &num);
+
+    // Check sign using nested if-else
+    if (num >= 0) {
+        if (num == 0) {
+            printf("The number is Zero.\n");
+        } else {
+            printf("%d is Positive.\n", num);
+        }
+    } else {
+        printf("%d is Negative.\n", num);
+    }
+
+    return 0;
+}
