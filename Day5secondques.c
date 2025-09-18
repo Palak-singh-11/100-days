0b0510fb31affa8f88306b4c0de3ba513082302f
Palak-singh-11 <anthenasingh11@gmail.com>
2025-09-19 00:24:32 +0530

    Add files via upload

---
+++ Day5secondques.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+int main() {
+    int totalSeconds;
+    int hours, minutes, seconds;
+
+    // Input from the user
+    printf("Enter time in seconds: ");
+    scanf("%d", &totalSeconds);
+
+    if (totalSeconds < 0) {
+        printf("Please enter a non-negative number of seconds.\n");
+        return 1;
+    }
+
+    // Calculate hours, minutes, seconds
+    hours = totalSeconds / 3600;
+    minutes = (totalSeconds % 3600) / 60;
+    seconds = totalSeconds % 60;
+
+    // Display the result
+    printf("%d seconds = %02d:%02d:%02d (hh:mm:ss)\n",
+           totalSeconds, hours, minutes, seconds);
+
+    return 0;
+}
