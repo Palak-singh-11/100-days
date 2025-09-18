c04bdc8ead862edf0a64d8532e27dacf59d5543c
Palak-singh-11 <anthenasingh11@gmail.com>
2025-09-18 23:53:37 +0530

    Add files via upload

---
+++ Day1secondques.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+int main(){
+    int a,b;
+    printf("Enter the value of a:");
+    scanf("%d",&a);
+    printf("Enter the value of b:");
+    scanf("%d",&b);
+    printf("The sum of a and b is %d",a+b);
+    printf("\nThe difference of a and b is %d",a-b);
+    printf("\nThe product of a and b is %d",a*b);
+    printf("\nThe division of a and b is %d",a/b);
+    return 0;
+}
