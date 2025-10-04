5e2c91d1a4b6f330e505cc1231a02e453ac75c7d
Palak-singh-11 <anthenasingh11@gmail.com>
2025-10-04 17:22:37 +0530

    Update Day01_A_sum of two integers.c

--- Day01_A_sum of two integers.c
+++ Day01_A_sum of two integers.c
@@ -1,4 +1,6 @@
 #include <stdio.h>
+//Write a program to input two numbers and display their sum.
+
 int main(){
     int a,b;
     printf("Enter the value of a:");
@@ -8,3 +10,4 @@ int main(){
     printf("The sum of a and b is %d",a+b);
     return 0;
 }
+
