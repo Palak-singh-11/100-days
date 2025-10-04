705a1e4a0317ef7798c680df2e1675a4408e64b4
Palak-singh-11 <anthenasingh11@gmail.com>
2025-10-04 20:30:22 +0530

    Update Day09_B_percentage and assigning.c

--- Day09_B_percentage and assigning.c
+++ Day09_B_percentage and assigning.c
@@ -1,3 +1,9 @@
+//Q18.Write a program that accepts a percentage (0-100) and assigns a grade based on the following criteria: 
+//90-100: Grade A 
+//80-89: Grade B 
+//70-79: Grade C 
+//60-69: Grade D 
+//below 60: Grade F.
 #include <stdio.h>
 int main() {
     int percentage;
@@ -27,3 +33,4 @@ int main() {
 
     return 0;
 }
+
