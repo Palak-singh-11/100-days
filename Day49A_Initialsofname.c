f2dc2f4bd4bd95910859f76c278733ee5322b1a4
Palak-singh-11 <anthenasingh11@gmail.com>
2025-11-16 17:48:30 +0530

    Implement initials printing from input name
    This program reads a name from input and prints the initials.

---
+++ Day49A_Initialsofname.c
@@ -0,0 +1,18 @@
+///Q97: Print the initials of a name.
+#include <stdio.h>
+int main() {
+    char str[1000];
+    int i = 0;
+    fgets(str, sizeof(str), stdin);
+    if (str[0] != ' ' && str[0] != '\n') {
+        printf("%c.", str[0]);
+    }
+    while (str[i] != '\0') {
+        if (str[i] == ' ' && str[i + 1] != ' ' && str[i + 1] != '\n' && str[i + 1] != '\0') {
+            printf("%c.", str[i + 1]);
+        }
+        i++;
+    }
+    printf("\n");
+    return 0;
+}
