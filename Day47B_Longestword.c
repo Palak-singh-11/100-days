f4a4dc2847cfaf6018e29baedaf3e3a73a810286
Palak-singh-11 <anthenasingh11@gmail.com>
2025-11-16 17:47:04 +0530

    Implement longest word finder in a sentence

---
+++ Day47B_Longestword.c
@@ -0,0 +1,27 @@
+///Q94: Find the longest word in a sentence.
+#include <stdio.h>
+#include <string.h>
+int main() {
+    char str[1000], word[100], longest[100];
+    int i = 0, j = 0, maxLen = 0;
+    fgets(str, sizeof(str), stdin);
+    while (str[i] != '\0' && str[i] != '\n') {
+        if (str[i] != ' ') {
+            word[j++] = str[i];
+        } else {
+            word[j] = '\0';
+            if (j > maxLen) {
+                maxLen = j;
+                strcpy(longest, word);
+            }
+            j = 0;
+        }
+        i++;
+    }
+    word[j] = '\0';
+    if (j > maxLen) {
+        strcpy(longest, word);
+    }
+    printf("%s\n", longest);
+    return 0;
+}
